f1f432fa740e412efea2f950ac955eaddee2b306
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-05-30 01:33:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,80 @@
 let box = document.getElementById('box');
-let img = [];
 let btn = document.querySelector('#btn');
+// let backimg = document.querySelector('.backimg');
+// let card = [];
+// let backcard = [];
+
 let showCard = false;
 
 function showhideCard() {
 
     showCard = !showCard;
-    
-    for (let i = 1; i <= 54; i++) {
-        box.innerHTML += `<img class="backimg" src="images/cardback.png" alt="Loading..">`;
+    box.innerHTML = '';
+
+    if (showCard) {
+
+        for (let i = 1; i <= 52; i++) {
+            box.innerHTML += `<img class="img" onclick="showhideCard();" src="images/cardback.png" alt="Loading..">`;
+            
+        }
         btn.innerText = "SHOW CARDs"
-    
-    }
-    if (!showCard) {
+        box.style.transition = '5s ease-in-out';  
+      }
+    else {
         for (let i = 1; i <= 13; i++) {
             box.innerHTML += `<img class="img" src="images/clubs_${[i]}.png" alt="Loading..">`;
-            img.push(box);
+            // card.push(box);
         }
 
         for (let i = 1; i <= 13; i++) {
 
             box.innerHTML += `<img class="img" src="images/diamonds_${[i]}.png" alt="Loading..">`;
-            img.push(box);
+            // card.push(box);
         }
 
         for (let i = 1; i <= 13; i++) {
             box.innerHTML += `<img class="img" src="images/hearts_${[i]}.png" alt="Loading..">`;
-            img.push(box);
+            // card.push(box);
         }
 
         for (let i = 1; i <= 13; i++) {
 
             box.innerHTML += `<img class="img" src="images/spades_${[i]}.png" alt="Loading..">`;
-            img.push(box);
+            // card.push(box);
         }
-        btn.innerText = "HIDE CARDs"
+        btn.innerText = "HIDE CARDs";
+
     }
 }
-console.log(img);
+console.log(card);
 
 // function hideCard() {
 
 // };
 
+// let img = document.querySelector('img');
+// let text = document.querySelector('#text');
+// let body = document.querySelector('body');
 
+// let isOn = false;
+// let count = 0;
+// function bulbOnOff() {
+//     count = count++;
+//     if (count >= 5) {
+//         body.innerHTML = `<h1>phat gaiii...</h1>`
+//     }
+//     isOn = !isOn;
+//     if (isOn) {
+//         img.src = "./on.png";
+//         text.innerText = "on";
+//         img.style.transition = ".2s ease-in-out";
+//     }
+//     else {
+//         img.src = "./off.webp";
+//         text.innerText = "off";
+//         img.style.transition = ".2s ease-in-out";
+//     }
+// }
 
 
 
