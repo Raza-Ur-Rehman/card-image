3acdb30d87005ae01077baf8ff8e7c984ff0c35f
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-05-29 11:16:42 +0500

    card function

---
+++ app.js
@@ -0,0 +1,48 @@
+let box = document.getElementById('box');
+let img = [];
+
+
+for (let i = 1; i <= 40; i++) { 
+    box.innerHTML += `<img onclick="showCard();" class="img" src="images/cardback.png" alt="Loading..">`;
+    img.push(box);
+    
+};
+function showCard() {
+for (let i = 1; i <= 13; i++) { 
+    box.innerHTML += `<img class="img" src="images/clubs_${[i]}.png" alt="Loading..">`;
+    img.push(box);
+};
+
+for (let i = 1; i <= 13; i++) {
+    
+    box.innerHTML += `<img class="img" src="images/diamonds_${[i]}.png" alt="Loading..">`;
+    img.push(box);
+};
+
+for (let i = 1; i <= 13; i++) {  
+    box.innerHTML += `<img class="img" src="images/hearts_${[i]}.png" alt="Loading..">`;
+    img.push(box);
+};
+
+for (let i = 1; i <= 13; i++) {
+    
+    box.innerHTML += `<img class="img" src="images/spades_${[i]}.png" alt="Loading..">`;
+    img.push(box);
+};
+
+};
+console.log(img);
+
+function hideCard() {
+
+};
+
+
+
+
+
+
+
+
+
+
