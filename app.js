79c3fb565f81f691186fdb8d778224cd8a89c86f
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-05-30 00:38:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
 let box = document.getElementById('box');
 let img = [];
-// let backImg = [];
+let btn = document.querySelector('#btn');
+let showCard = false;
 
-// function hideCard() {
-//     img.pop();
-//     // for (let i = 1; i <= 40; i++) { 
-        
-//     //     box.innerHTML += `<img class="img" src="images/cardback.png" alt="Loading..">`;
-//     //     backimg.push(box);
-        
-//     // }
-// };
-function showCard() {
-for (let i = 1; i <= 13; i++) { 
-    box.innerHTML += `<img class="img" src="images/clubs_${[i]}.png" alt="Loading..">`;
-    img.push(box);
-};
+function showhideCard() {
 
-for (let i = 1; i <= 13; i++) {
+    showCard = !showCard;
     
-    box.innerHTML += `<img class="img" src="images/diamonds_${[i]}.png" alt="Loading..">`;
-    img.push(box);
-};
-
-for (let i = 1; i <= 13; i++) {  
-    box.innerHTML += `<img class="img" src="images/hearts_${[i]}.png" alt="Loading..">`;
-    img.push(box);
-};
-
-for (let i = 1; i <= 13; i++) {
+    for (let i = 1; i <= 54; i++) {
+        box.innerHTML += `<img class="backimg" src="images/cardback.png" alt="Loading..">`;
+        btn.innerText = "SHOW CARDs"
     
-    box.innerHTML += `<img class="img" src="images/spades_${[i]}.png" alt="Loading..">`;
-    img.push(box);
-};
+    }
+    if (!showCard) {
+        for (let i = 1; i <= 13; i++) {
+            box.innerHTML += `<img class="img" src="images/clubs_${[i]}.png" alt="Loading..">`;
+            img.push(box);
+        }
 
-};
+        for (let i = 1; i <= 13; i++) {
+
+            box.innerHTML += `<img class="img" src="images/diamonds_${[i]}.png" alt="Loading..">`;
+            img.push(box);
+        }
+
+        for (let i = 1; i <= 13; i++) {
+            box.innerHTML += `<img class="img" src="images/hearts_${[i]}.png" alt="Loading..">`;
+            img.push(box);
+        }
+
+        for (let i = 1; i <= 13; i++) {
+
+            box.innerHTML += `<img class="img" src="images/spades_${[i]}.png" alt="Loading..">`;
+            img.push(box);
+        }
+        btn.innerText = "HIDE CARDs"
+    }
+}
 console.log(img);
 
 // function hideCard() {
